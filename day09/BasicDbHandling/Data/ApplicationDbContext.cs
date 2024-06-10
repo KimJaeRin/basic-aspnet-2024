56e8d8fc78be320429a9c86ab4cad541bea3e5cd
KimJaeRin <hello6234@naver.com>
2024-06-10 16:29:09 +0900

    6월 10일

---
+++ day09/BasicDbHandling/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BasicDbHandling.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicDbHandling.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+            {
+
+            }
+
+        public DbSet<Category> Categories { get; set; }
+       
+    }
+}
---
+++ day09/BasicDbHandling/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace BasicDbHandling.Models
+{
+    /// <summary>
+    /// DB에 테이블로 만들어지는 엔티티 클래스
+    /// </summary>
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DispalyOrder {  get; set; }
+        public DateTime PostDate { get; set; } = DateTime.Now; // 기본적으로 생성되는 현재날짜
+    }
+}
