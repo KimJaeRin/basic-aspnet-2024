6b477db9485c55131a30d252220b153485877fc7
KimJaeRin <hello6234@naver.com>
2024-07-18 09:39:59 +0900

    v

---
+++ day10/MyPortfolio/Models/Board.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyPortfolio.Models
+{
+    public class Board
+    {
+        [Key]
+        public int Id { get; set; }             // PK
+
+        [Required]
+        [MaxLength(50)]     // NVARCHAR(50) 사이즈 지정하려면!
+        public string Name { get; set; }        // 작성자명
+
+        [MaxLength(20)]
+        public string UserId { get; set; }      // 작성자 ID
+
+        [Required]
+        [MaxLength(250)]
+        public string title { get; set; }       // 게시글 제목
+
+        public string Contents { get; set; }    // 게시글 내용
+
+        public int Hit { get; set; }            // 게시글 읽은 횟수
+
+        public DateTime RegDate { get; set; } = DateTime.Now;   // 게시글 작성일자
+
+        public DateTime ModDate { get; set; }  // 게시글 최종 수정일자
+
+    }
+}
